d18e1214b10da5136e49588d3adf7cfaa995051d
Wyatt Johnson <accounts+github@wyattjoh.ca>
2022-04-08 11:34:35 -0600

    feat: added origin pinning

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,9 @@ export default function Home() {
   return (
     <div className="">
       <div>Home</div>
-      <iframe src="/api/preview/enable"></iframe>
+      <iframe
+        src={`${process.env.NEXT_PUBLIC_PREVIEW_ORIGIN}/api/preview/enable`}
+      />
     </div>
   );
 }
